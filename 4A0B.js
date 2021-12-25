83ed1c90bea1e7f0c985650e586980c7c8f66ee7
KateChu <chu.shihchien@gmail.com>
2021-12-25 11:13:01 +0800

    rule modal

--- 4A0B.js
+++ 4A0B.js
@@ -95,9 +95,28 @@ showAns.addEventListener('click', (e) => {
 
 newGame.addEventListener('click', (e) => {
   submit.disabled = false;
+  guessInput.value = ''
   guessCorrection = '0A0B'
   generateNewAnswer()
   return ansScreen.innerText = guessCorrection
 })
 
 
+// Modal part
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("ruleBtn");
+const close = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+
+close.onclick = function () {
+  modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
